f22e8f90f03bc726810ca204592d84877248124f
suzuki119 <suzukiyutaro119@gmail.com>
2026-05-26 13:39:31 +0900

    Add login, logout, and product search functionality
    - Created login-input.php for user login form.
    - Implemented login-output.php to handle login logic and session management.
    - Added logout-input.php to confirm logout action.
    - Developed logout-output.php to process logout and clear session.
    - Introduced menu.php for navigation links.
    - Created product.php for product search functionality with keyword input.
    - Established database connection for user authentication and product retrieval.

---
+++ php-test/chapter6/edit2.php
@@ -0,0 +1,38 @@
+<?php require '../header.php'; ?>
+
+
+<div class="th0">id</div>
+<div class="th1">商品名</div>
+<div class="th1">価格</div>
+<?php
+
+$pdo = new PDO('mysql:host=localhost;dbname=shop;charset=utf8', 'staff', 'password');
+
+foreach ($pdo->query('select * from product') as $row) {
+    echo '<form class="ib" action="edit3.php" method="post">';
+    echo '<input type="hidden" name="command" value="update">';
+    echo '<input type="hidden" name="id" value="', $row['id'], '">';
+    echo '<div class="td0">', $row['id'], '</div>';
+    echo '<div class="td1">', $row['name'], '</div>';
+    echo '<div class="td1">', $row['price'], '円</div>';
+    echo '<div class="td2">',
+    '<input type="submit" value="更新">', '</div>';
+    echo '</form>';
+    echo '<form class="ib" action="edit3.php" method="post">';
+    echo '<input type="hidden" name="command" value="delete">';
+    echo '<input type="hidden" name="id" value="', $row['id'], '">';
+    echo '<input type="submit" value="削除">', '</div>';
+    echo '</form>';
+    echo "\n";
+}
+
+?>
+
+<form action="edit3.php" method="post">
+    <input type="hidden" name="command" value="insert">
+    </div>
+    <div class="td1"><input type="text" name="name"></div>
+    <div class="td1"><input type="text" name="price"></div>
+    <div class="td2"><input type="submit" value="追加"></div>
+</form><br>
+<?php require '../footer.php'; ?>
---
+++ php-test/chapter7/login-input.php
@@ -0,0 +1,10 @@
+<?php require '../header.php'; ?>
+<?php require 'menu.php'; ?>
+<form action="login-output.php" method="post">
+    <input type="text" name="login">
+    <input type="password" name="password">
+    <input type="submit" value="ログイン">
+</form>
+
+
+<?php require '../footer.php'; ?>
