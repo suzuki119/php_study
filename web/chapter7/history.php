0e20d442d4b1d0e276b9cee52dfb260a3ed089d9
suzuki119 <suzukiyutaro119@gmail.com>
2026-06-19 14:27:32 +0900

    購入履歴表示機能を実装し、税率編集機能を追加しました。また、CSSスタイルを整理しました。

--- web/chapter7/history.php
+++ web/chapter7/history.php
@@ -2,10 +2,53 @@
 <?php require '../header.php'; ?>
 <?php require 'menu.php'; ?>
 <?php
-	$pdo=new PDO('mysql:host=localhost;dbname=shop;charset=utf8', 
-		'staff', 'password');
+if (isset($_SESSION['customer'])) {
 
-// ログインしている場合のみ購入履歴を表示
+	//
+	$pdo = new PDO(
+		'mysql:host=localhost;dbname=shop;charset=utf8',
+		'staff',
+		'password'
+	);
+
+	$sql_purchase = $pdo->prepare(
+		'select * from purchase where customer_id=? order by id desc'
+	); // purchaseテーブルに繋げ、purchase_detailテーブルと結合して、購入の詳細情報を取得するためのSQL文を準備しています。purchase_idでフィルタリングし、productテーブルと結合して、商品情報も取得しています。
+
+	$sql_purchase->execute([$_SESSION['customer']['id']]);
+	foreach ($sql_purchase as $row_purchase) {
+
+		$sql_detail = $pdo->prepare(
+			'select * from purchase_detail,product ' .
+				'where purchase_id=? and product_id=id'
+		);
+		$sql_detail->execute([$row_purchase['id']]);
+
+		echo '<table>';
+		echo '<tr><th>商品番号</th><th>商品名</th>',
+		'<th>価格(税抜)</th><th>個数</th><th>小計</th></tr>';
+		$total = 0;
+
+		foreach ($sql_detail as $row_detail) {
+			echo '<tr>';
+			echo '<td>', $row_detail['id'], '</td>';
+			echo '<td><a href="detail.php?id=', $row_detail['id'], '">',
+			$row_detail['name'], '</a></td>';
+			echo '<td>', $row_detail['price'], '</td>';
+			echo '<td>', $row_detail['count'], '</td>';
+			$subtotal = $row_detail['price'] * $row_detail['count'];
+			$total += $subtotal;
+			echo '<td>', $subtotal, '</td>';
+			echo '</tr>';
+		}
+		echo '<tr><td>合計</td><td></td><td></td><td></td><td>',
+		$total, '</td></tr>';
+		echo '</table>';
+		echo '<hr>';
+	}
+} else {
+	echo '購入履歴を表示するには、ログインしてください。';
+}
 // purchase テーブルを、セッションの顧客idで絞り込む
 // →絞り込み結果の購入id(複数ありえる)で、purchase_detailを絞り込む
 // →purchase_detailから取得した商品名(複数ありえる)、個数、金額を表示する
