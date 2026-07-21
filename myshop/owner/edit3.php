59c8c8925b668c4df065f0a32a33df03e6c08d6a
suzuki119 <suzukiyutaro119@gmail.com>
2026-07-21 14:13:52 +0900

    商品画像のアップロード機能を追加し、商品編集フォームに画像選択オプションを実装しました。また、スタイルを調整し、商品表示を改善しました。

--- myshop/owner/edit3.php
+++ myshop/owner/edit3.php
@@ -3,6 +3,20 @@
 
 $pdo = new PDO('mysql:host=localhost;dbname=shop;charset=utf8', 'staff', 'password');
 
+function save_product_image($id)
+{
+    if (!isset($_FILES['image']) || $_FILES['image']['error'] === UPLOAD_ERR_NO_FILE) {
+        return;
+    }
+    if ($_FILES['image']['error'] !== UPLOAD_ERR_OK) {
+        return;
+    }
+    if (@getimagesize($_FILES['image']['tmp_name']) === false) {
+        return;
+    }
+    move_uploaded_file($_FILES['image']['tmp_name'], '../user/image/' . $id . '.jpg');
+}
+
 if (isset($_REQUEST['command'])) {
     switch ($_REQUEST['command']) {
         case 'insert';
@@ -12,6 +26,7 @@ if (isset($_REQUEST['command'])) {
             ) break;
             $sql = $pdo->prepare('insert into product values(null, ?, ?)');
             $sql->execute([$_REQUEST['name'], $_REQUEST['price']]);
+            save_product_image($pdo->lastInsertId());
             break;
         case 'update';
             if (
@@ -22,6 +37,7 @@ if (isset($_REQUEST['command'])) {
             $sql->execute(
                 [htmlspecialchars($_REQUEST['name']), $_REQUEST['price'], $_REQUEST['id']]
             );
+            save_product_image($_REQUEST['id']);
             break;
         case 'delete';
             $sql = $pdo->prepare('delete from product where id=?');
@@ -31,31 +47,39 @@ if (isset($_REQUEST['command'])) {
 }
 
 foreach ($pdo->query('select * from product') as $row) {
-    echo '<form class="ib" action="edit3.php" method="post">';
+    echo '<div class="product-row">';
+    echo '<form class="ib" action="edit3.php" method="post" enctype="multipart/form-data">';
     echo '<input type="hidden" name="command" value="update">';
     echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
     echo '<div class="td0">' . $row['id'] . '</div>';
     echo '<div class="td1"><input type="text" name="name" value="' . htmlspecialchars($row['name']) . '"></div>';
-    echo '<div class="td1"><input type="text" name="price" value="' . $row['price'] . '"></div>';
-    echo '<div class="td2"><input type="submit" name="update" value="更新"></div>';
+    echo '<div class="td1"><input type="text" name="price" value="' . $row['price'] . '"></div><br>';
+
+    echo '<div><div class="td1"><img src="../user/image/' . $row['id'] . '.jpg" alt="' . htmlspecialchars($row['name']) . '" width="60" onerror="this.remove()"></div>';
+    echo '<div class="td1"><input type="file" name="image" accept="image/*"></div></div>';
+
     echo '</form>';
+
 
     echo '<form class="ib" action="edit3.php" method="post">';
     echo '<input type="hidden" name="command" value="delete">';
     echo '<input type="hidden" name="id" value="', $row['id'], '">';
     echo '<input type="submit" name="delete" value="削除">';
+    echo '<input type="submit" name="update" value="更新">';
     echo '</form><br>';
     echo "\n";
+    echo '</div>';
 }
 
 
 ?>
 
-<form action="edit3.php" method="post">
+<form action="edit3.php" method="post" enctype="multipart/form-data">
     <input type="hidden" name="command" value="insert">
     <div class="td0"></div>
     <div class="td1"><input type="text" name="name"></div>
     <div class="td1"><input type="text" name="price"></div>
+    <div class="td1"><input type="file" name="image" accept="image/*"></div>
     <div class="td2"><input type="submit" value="追加"></div>
 </form>
 <br>
